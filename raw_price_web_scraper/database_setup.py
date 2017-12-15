56c9681b7e8cd62f6416b20ab76aa588d19bb4ee
vagrant <vagrant@vagrant.vm>
2017-12-15 17:54:58 +0000

    Add database_setup file for raw prices and information

---
+++ raw_price_web_scraper/database_setup.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+engine = create_engine(os.environ['DATABASE_URL'])
+
+class StockInformation(Base):
+    __tablename__ = 'stock_information'
+
+    id = Column(String(10), primary_key=True)
+    stock_name = Column(String(250), nullable=False)
+    description = Column(Text)
+    sector = Column(String(50))
+    stock_index =  Column(String(10))
+
+class DailyStockPrices(Base):
+    __tablename__ = 'daily_stock_price'
+
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(String(10), ForeignKey('stock_information.id'))
+    open = Column(Float)
+    close = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    volume = Column(Float)
+    change = Column(Float)
+    change_percent = Column(Float)
+
+    stock_information = relationship(StockInformation)
+
+class CurrentStockPrice(Base):
+    __tablename__ = 'current_stock_price'
+
+    id = Column(Integer, primary_key=True)
+    stock_id = Column(String(10), ForeignKey('stock_information.id'))
+    time_dt = Column(DateTime)
+    price = Column(Float)
+
+    stock_information = relationship(StockInformation)
+
+
+Base.metadata.create_all(engine)
+
